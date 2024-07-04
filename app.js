00c624d6f2c6fe77de4e31e81abf8a1dbd572cbe
unknown <senacelularnet@gmail.com>
2024-07-04 22:28:46 +0200

    Calculadora de prentamos V0.1

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+const montoCapital = document.getElementById("monto"); 
+const meses = document.getElementById("meses"); 
+const insteres = document.getElementById("Interes"); 
+const btn = document.getElementById("btn"); 
+const resultado = document.getElementById("contenedor"); 
+const text = document.getElementById("cuota"); 
+
+
+
+
+
+btn.addEventListener("click" , calculadoraPrestamos);
+
+
+function calculadoraPrestamos(){
+    let cap = montoCapital.value;
+    let int = insteres.value;
+    let me = meses.value
+    let total = 0;
+    let suma = 0;
+
+   total = cap * int; 
+   total = + cap + total;
+   suma = total / me;  
+    
+
+    text.textContent = "Sus prestamos a Pagar "
+    resultado.innerHTML = suma
+    resultado.style.height = "200px";
+    resultado.style.width= "70%";
+  
+}
+
+
+
